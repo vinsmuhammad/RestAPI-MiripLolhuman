11ee9a65350f67c9dac8e4ab5cb9f0ca378d42e4
vinsmuhammad <99631718+vinsmuhammad@users.noreply.github.com>
2023-04-09 00:29:37 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -22,8 +22,8 @@ var lolkill = db.get("lolkill");
 	console.log('')
 }
 
-var creator = 'HelgaZex' // ubah jadi nama lu
-const listkey = ["HelgaZex","HelgaZex","HelgaZex"]; // ubah apikey nya, tambah aja klo mau
+var creator = 'garvincg' // ubah jadi nama lu
+const listkey = ["key","key","key"]; // ubah apikey nya, tambah aja klo mau
 
 var axios = require('axios');
 var qs = require('qs');
